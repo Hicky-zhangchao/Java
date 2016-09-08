7fa9b14b2b7c2d1bb47e8c70467a5d4cd2d09f1e
huni1lian <iloveyou9ai@live.com>
2016-09-08 18:30:07 +0800

    10000以内质数之和

--- demo1/src/main/java/java8.java
+++ demo1/src/main/java/java8.java
@@ -4,5 +4,21 @@
 public class java8 {
     public static void main(String[] args){
 
-    }
+
+            int sum=2;//1既不是质数2是质数
+            int i,j;
+            for(i=3;i<10000;i++)
+            {
+                for(j=2;j<i;j++)
+                {
+                    if(i%j==0)
+                        break;
+
+                }
+                if(j==i)
+                    sum=sum+i;
+            }
+            System.out.println(sum);
+        }
+
 }
